a165a01143304dc739625c344ec5a2a2a53e8354
alberto96A <albertofassbender96@gmail.com>
2023-06-12 13:01:03 +0200

    Edición1

--- taxi.py
+++ taxi.py
@@ -27,5 +27,28 @@ def main():
         else:
             print("Instrucción inválida.")
 
+
+            import time
+
+def calcular_tarifa(taxi_en_movimiento):
+    tarifa_por_segundo = 0.02 if not taxi_en_movimiento else 0.05
+    tiempo_inicio = time.time()  # Obtiene el tiempo de inicio en segundos
+    tiempo_actual = time.time()
+
+    while True:
+        tiempo_transcurrido = tiempo_actual - tiempo_inicio
+        tarifa_actual = tiempo_transcurrido * tarifa_por_segundo
+        print(f'Tarifa actual: {tarifa_actual:.2f} euros')
+
+        # Puedes agregar aquí lógica adicional si necesitas detener el cálculo en ciertas condiciones
+
+        tiempo_actual = time.time()
+        time.sleep(1)  # Pausa por 1 segundo antes de la siguiente iteración
+
+# Ejemplo de uso
+taxi_movimiento = False  # Cambia esto a True o False según corresponda
+
+calcular_tarifa(taxi_movimiento)
+
 if __name__ == "__main__":
     main()
