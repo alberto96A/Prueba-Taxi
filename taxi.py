2afa44ee864b9d561e2d6563ed185ea55c2cf1dd
BlanckSpeed <inrodrigoant@gmail.com>
2023-06-15 09:15:08 +0200

    registroytest

--- taxi.py
+++ taxi.py
@@ -78,3 +78,26 @@ def main():
 
 if __name__ == "__main__":
     main()
+import unittest
+import time
+from io import StringIO
+from contextlib import redirect_stdout
+from your_module import calcular_tarifa
+
+class TestCalcularTarifa(unittest.TestCase):
+
+    def test_calcular_tarifa(self):
+        taxi_en_movimiento = False
+        tiempo_inicio = time.time()
+
+        # Capturar la salida de la función calcular_tarifa
+        with StringIO() as output:
+            with redirect_stdout(output):
+                calcular_tarifa(taxi_en_movimiento, tiempo_inicio)
+                output_str = output.getvalue().strip()
+
+        # Verificar si la salida es correcta
+        self.assertEqual(output_str, "Tarifa actual: 0.00 euros")
+
+if __name__ == '__main__':
+    unittest.main()
