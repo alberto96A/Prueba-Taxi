5ba3f12c31acd9ba2108f2eec14d2983008762ac
BlanckSpeed <inrodrigoant@gmail.com>
2023-06-30 14:03:35 +0200

    showpy

--- taxi.py
+++ taxi.py
@@ -5,6 +5,7 @@ import logging
 logging.basicConfig(filename='registro.log', level=logging.INFO)
 
 def calcular_tarifa(taxi_en_movimiento, tiempo_inicio):
+    """Calcula la tarifa por segundo dependiendo si el taxi está en movimiento o no."""
     # Calcula la tarifa por segundo dependiendo si el taxi está en movimiento o no
     tarifa_por_segundo = 0.02 if not taxi_en_movimiento else 0.05
     tiempo_actual = time.time()
@@ -13,19 +14,20 @@ def calcular_tarifa(taxi_en_movimiento, tiempo_inicio):
     print(f'Tarifa actual: {tarifa_actual:.2f} euros')
 
 def guardar_registro(registro):
+    """Guarda el registro de la carrera en un archivo de texto."""
     # Abre el archivo "registro.txt" en modo de escritura y agrega el registro al final
     with open('registro.txt', 'a') as file:
         file.write(registro + '\n')
 
 def consultar_registro():
+    """Consulta y muestra el registro histórico de carreras desde un archivo de texto."""
     # Abre el archivo "registro.txt" en modo de lectura y muestra su contenido
     with open('registro.txt', 'r') as file:
         registros = file.read()
         print(registros)
 
-def main():
-    # Mensajes de bienvenida e instrucciones del programa
-    print("Bienvenido al programa de cálculo de tarifa del taxi.")
+def mostrar_instrucciones():
+    """Muestra las instrucciones del programa."""
     print("Instrucciones:")
     print("- Para iniciar una carrera, ingrese 'empezar'")
     print("- Para indicar que el taxi está en movimiento, ingrese 'movimiento'")
@@ -33,6 +35,12 @@ def main():
     print("- Para finalizar la carrera y obtener el precio total, ingrese 'finalizar'")
     print("- Para consultar el registro histórico de carreras, ingrese 'registro'")
     print("- Para salir del programa, ingrese 'salir'")
+
+def main():
+    # Mensaje de bienvenida
+    print("Bienvenido al programa de cálculo de tarifa del taxi.")
+
+    mostrar_instrucciones()
 
     taxi_en_movimiento = False
     carrera_iniciada = False
